b27ea58e9e6ce833924a6845e65905d485bb4041
D7706 <D7706@DESKTOP-R1K8PBU>
2018-10-24 20:04:59 +0900

    모의 중간고사

--- 201412360-midExamSample/src/main/java/kr/ac/dit/controller/MemberController.java
+++ 201412360-midExamSample/src/main/java/kr/ac/dit/controller/MemberController.java
@@ -5,24 +5,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import kr.ac.dit.domain.StudentVO;
-import kr.ac.dit.service.StudentService;
+import kr.ac.dit.domain.MemberVO;
+import kr.ac.dit.service.MemberService;
+
 @Controller
-public class StudentController {
+public class MemberController {
 	@Autowired
-	private StudentService studentService;
+	private MemberService memberService;
 	@RequestMapping(value="create", method=RequestMethod.GET)
 	public String createGET() throws Exception {
 		return "createMember";
 	}
 	@RequestMapping(value="create", method=RequestMethod.POST)
-	public String createPOST(StudentVO studentVO, Model model) throws Exception {
-		studentService.createStudent(studentVO);
+	public String createPOST(MemberVO memberVO, Model model) throws Exception {
+		memberService.createMember(memberVO);
 		return "createMember";
 	}
 	@RequestMapping("read")
 	public String readList(Model model) throws Exception {
-		model.addAttribute("items", studentService.readStudentList());
+		model.addAttribute("items", memberService.readMemberList());
 		return "readMember";
 	}
 	
