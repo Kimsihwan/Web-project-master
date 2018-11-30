97e87e0324fa9afa71c6f6692310d77c4c970796
Kimsihwan <namid882@gmail.com>
2018-11-30 23:57:50 +0900

    8주차 전반 종합 예제(게시판 로그온)

---
+++ 201412360-BoardLogon/src/main/java/kr/ac/dit/domain/LogonDTO.java
@@ -0,0 +1,22 @@
+package kr.ac.dit.domain;
+
+import java.security.Timestamp;
+
+public class LogonDTO {
+	 private String id;
+	 private String pwd;
+	 
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	 
+}
